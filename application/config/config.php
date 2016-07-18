cac119fe284dcddaeb2a67500123cd6636ea22d7
Sujan Karki <regolithsjk@gmail.com>
2016-07-18 09:38:34 +0545

    sess_save_path set. model ion auth capatilized

--- application/config/config.php
+++ application/config/config.php
@@ -29,11 +29,11 @@ $config['modules_locations'] = array(
 );
 if($_SERVER['HTTP_HOST']=="localhost" || $_SERVER['HTTP_HOST']=="127.0.1.1")
 {
-    $config['base_url']='http://localhost/brittsorganic/';
+    $config['base_url']='http://localhost/yoursite/';
 }
 
 else{
-    $config['base_url'] = 'http://dac.com.np/auto/';
+    $config['base_url'] = 'http://yoursite/';
 }
 
 /*
@@ -381,7 +381,7 @@ $config['encryption_key'] = '';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = sys_get_temp_dir ();
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
